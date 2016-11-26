757ac60a7c6a9cd52dc36469264adbf11ac35842
JeremiahFly <jfly1@uco.edu>
2016-11-25 20:15:56 -0600

    Adding superpoker server

---
+++ SuperPokerServer/src/SuperPokerServer/Controllers/SuperPokerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Ringmaster.Models;
+using Ringmaster.Service;
+
+namespace Ringmaster.Controllers
+{
+    [Route("api/[controller]")]
+    public class SuperPokerController : Controller
+    {
+        private SuperPokerService SuperPokerService { get; set; }
+
+        public SuperPokerController(SuperPokerService superPokerService)
+        {
+            this.SuperPokerService = superPokerService;
+        }
+
+        [HttpGet]
+        public SuperPokerDeck Get()
+        {
+            this.SuperPokerService.AddPlayer(this.Request.Host.Value);
+
+            return this.SuperPokerService.GameStart();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] SuperPokerDeck newDeck)
+        {
+
+        }
+    }
+}
---
+++ SuperPokerServer/src/SuperPokerServer/Service/SuperPokerService.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using Ringmaster.Models;
+using System.Linq;
+using System;
+
+namespace Ringmaster.Service
+{
+    public class SuperPokerService
+    {
+        private static Game Game { get; set; }
+
+        public void AddPlayer(string hostName)
+        {
+            var game = this.GetCurrentGame();
+
+            game.AddPlayer(hostName);
+        }
+
+        public SuperPokerDeck GameStart()
+        {
+            // Return deck when all players are ready
+
+            return this.GetCurrentGame().GetGameDeck();
+        }
+
+        private Game GetCurrentGame()
+        {
+            if (Game == null) Game = new Game();
+            return Game;
+        }
+
+        public void SetCurrentDeck(SuperPokerDeck newDeck)
+        {
+            if (Game == null) Game = new Game();
+            Game.SetGameDeck(newDeck);
+        }
+
+        private CardValue ConvertToCardvalue(int c1)
+        {
+            switch (c1)
+            {
+                case 1:
+                    return CardValue.Ace;
+                case 2:
+                    return CardValue.Two;
+                case 3:
+                    return CardValue.Three;
+                case 4:
+                    return CardValue.Four;
+                case 5:
+                    return CardValue.Five;
+                case 6:
+                    return CardValue.Six;
+                case 7:
+                    return CardValue.Seven;
+                case 8:
+                    return CardValue.Eight;
+                case 9:
+                    return CardValue.Nine;
+                case 10:
+                    return CardValue.Ten;
+                case 11:
+                    return CardValue.Jack;
+                case 12:
+                    return CardValue.Queen;
+                case 13:
+                    return CardValue.King;
+                default:
+                    throw new Exception("Attempted to create card without value");
+            }
+        }
+    }
+}
